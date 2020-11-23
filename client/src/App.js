ded54232d9120bdf9b89af8da40231ba351f579b
Naasgul <lnaas@hotmail.de>
2020-11-23 18:14:57 +0100

    Add link to storybook in app

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Button from "./stories/Button";
+import Button from "./components/Button";
 
 function App() {
   return (
@@ -18,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <a href="/storybook">To Storybook</a>
         <Button />
       </header>
     </div>
