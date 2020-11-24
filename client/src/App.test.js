7d0c9649684af4f9cbdd5aaf0a62f69073fe668b
Naasgul <lnaas@hotmail.de>
2020-11-24 09:48:28 +0100

    Fix test for storybook link

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders storybook react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/To Storybook/i);
   expect(linkElement).toBeInTheDocument();
 });
