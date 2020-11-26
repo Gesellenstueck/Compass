adce544d2edae9506021cc0b23a85407e4e1b62a
Naasgul <lnaas@hotmail.de>
2020-11-26 10:00:01 +0100

    🚧  Imports from React-Router-Dom and add <Router>

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,30 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Button from "./components/Button";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a href="/storybook">To Storybook</a>
-        <Button />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <a href="/storybook">To Storybook</a>
+          <Button />
+        </header>
+      </div>
+    </Router>
   );
 }
 
