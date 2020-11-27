a949bdc7e6b93401ace26e4cebe721376654567d
Naasgul <lnaas@hotmail.de>
2020-11-27 10:54:37 +0100

    💄  Add oxygen-font to body

--- client/src/GlobalStyles.js
+++ client/src/GlobalStyles.js
@@ -1,7 +1,29 @@
 import { createGlobalStyle } from "styled-components";
 //hier auch Google-Fonts Import
+import OxygenBold from "./assets/fonts/Oxygen-Bold.ttf";
+import OxygenRegular from "./assets/fonts/Oxygen-Regular.ttf";
 
 const GlobalStyles = createGlobalStyle`
+*,*::before,*::after{
+  box-sizing:border-box;
+  }
+  :root{
+
+  }
+html {
+
+}
+  body {
+      padding:0;
+      margin:0;
+      @font-face {
+    font-family: "Oxygen-Regular";
+    src: url(${OxygenRegular}) format("truetype");
+    
+  }
+  font-family:"Oxygen-Regular"
+  }
+
 
 
 
