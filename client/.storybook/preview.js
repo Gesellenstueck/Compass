bd10f49123380918e4640a29977cdbbc6d2339ec
Naasgul <lnaas@hotmail.de>
2020-11-27 16:58:56 +0100

    ✨  Add GlobalStyles to storybook

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,3 +1,12 @@
+import GlobalStyles from "../src/GlobalStyles";
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyles />
+      <Story />
+    </>
+  ),
+];
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 };
