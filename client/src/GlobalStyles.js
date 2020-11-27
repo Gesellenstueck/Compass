09d9228668ac30f30b1d2530b1e0f95563b000a4
Naasgul <lnaas@hotmail.de>
2020-11-27 11:16:18 +0100

    💄  Add oxygen-bold to headlines in globalStyles

--- client/src/GlobalStyles.js
+++ client/src/GlobalStyles.js
@@ -10,29 +10,34 @@ const GlobalStyles = createGlobalStyle`
   :root{
 
   }
-html {
 
+
+html {
   @font-face {
     font-family: "Oxygen-Regular";
     src: url(${OxygenRegular}) format("truetype");
     
   }
   font-family:"Oxygen-Regular";
-  font-size:16px;
+  font-size:16px;   
+     }
 
-  
 
-      
-     
-
-}
   body {
     padding:0;
       margin:0;
   
   }
 
-
+h1, h2, h3, h4 {
+  @font-face {
+    font-family: "Oxygen-Bold";
+    src: url(${OxygenBold}) format("truetype");
+    
+  }
+  font-family:"Oxygen-Bold";
+  
+}
 
 
 `;
