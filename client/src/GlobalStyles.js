5ef54d2a6ca76e9600aa86688be61ce1d1abc1de
Naasgul <lnaas@hotmail.de>
2020-11-27 16:58:09 +0100

    🩹  Add fontFace to GlobalStyles again

--- client/src/GlobalStyles.js
+++ client/src/GlobalStyles.js
@@ -1,5 +1,5 @@
 import { createGlobalStyle } from "styled-components";
-
+//hier auch Google-Fonts Import
 import OxygenBold from "./assets/fonts/Oxygen-Bold.ttf";
 import OxygenRegular from "./assets/fonts/Oxygen-Regular.ttf";
 
@@ -29,23 +29,31 @@ const GlobalStyles = createGlobalStyle`
       height: 100vw;
     }
 
-
-html {
-
-  font-family:"Oxygen-Regular";
-  font-size:16px;   
- 
-     }
-
-
-  body {
-    padding:0;
+    html {
+      @font-face {
+        font-family: "Oxygen-Regular";
+        src: url(${OxygenRegular}) format("truetype");
+        
+      } 
+      font-family:"Oxygen-Regular";
+      font-size:16px;   
+      
+    }
+    
+    
+    body {
+      padding:0;
       margin:0;
-  background: var(--background-primary)
-  }
-
-h1, h2, h3, h4 {
-  font-family:"Oxygen-Bold";
+      background: var(--background-primary)
+    }
+    
+    h1, h2, h3, h4 {
+      @font-face {
+         font-family: "Oxygen-Bold";
+         src: url(${OxygenBold}) format("truetype");
+         
+         font-family:"Oxygen-Bold";
+       }  
   
 }
 
@@ -53,7 +61,8 @@ button {
   outline: none;
   cursor: pointer;  
   border:none;
-  font-family:"Oxygen-Bold"
+  font-family:"Oxygen-Bold";
+  font-size:1.25rem;
   
 }
 
