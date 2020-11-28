388050aa2183f74bd856b59c538e70bb982d4ea2
Naasgul <lnaas@hotmail.de>
2020-11-28 16:17:47 +0100

    🚧 Add new Card component with copy of button

---
+++ client/src/components/Card/Button.js
@@ -0,0 +1,66 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const sizes = {
+  small: {
+    width: "30vw",
+    height: "1rem",
+    font: "Oxygen-Regular",
+  },
+  medium: {},
+  large: {
+    width: "40vw",
+    height: "2rem",
+    font: "Oxygen-Bold",
+  },
+};
+
+const SubmitButton = styled.button`
+  border-radius: 1.875rem;
+  text-transform: uppercase;
+  color: var(--emphasis-color);
+  background: var(--basic-color);
+  font-family: ${(props) => sizes[props.size].font};
+  :active {
+    background: var(--emphasis-color);
+    color: var(--basic-color);
+  }
+  width: ${(props) => sizes[props.size].width};
+  height: ${(props) => sizes[props.size].height};
+`;
+
+/**
+ * Primary UI component for user interaction
+ */
+const Button = ({ backgroundColor, size, label, ...props }) => {
+  return <SubmitButton size={size}>{label}</SubmitButton>;
+};
+
+Button.propTypes = {
+  /**
+   * What background color to use
+   */
+  backgroundColor: PropTypes.string,
+  /**
+   * How large should the button be?
+   */
+  size: PropTypes.oneOf(["small", "medium", "large"]),
+  /**
+   * Button contents
+   */
+  label: PropTypes.string.isRequired,
+  /**
+   * Optional click handler
+   */
+  onClick: PropTypes.func,
+};
+
+Button.defaultProps = {
+  backgroundColor: null,
+  primary: false,
+  size: "medium",
+  onClick: undefined,
+};
+
+export default Button;
