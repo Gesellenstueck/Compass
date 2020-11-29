01c31605372e0431fe4fc9e20eb2ca5c595149c8
Naasgul <lnaas@hotmail.de>
2020-11-29 10:51:12 +0100

    🔥 Deleting of unecessary storybook code

--- client/src/components/Button/Button.stories.js
+++ client/src/components/Button/Button.stories.js
@@ -12,17 +12,6 @@ export default {
 
 const Template = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
-  primary: true,
-  label: "Button",
-};
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: "Button",
-};
-
 export const Large = Template.bind({});
 Large.args = {
   size: "large",
--- client/src/components/Card/Card.stories.js
+++ client/src/components/Card/Card.stories.js
@@ -12,25 +12,8 @@ export default {
 
 const Template = (args) => <Card {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
-  primary: true,
-  label: "Button",
-};
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: "Button",
-};
-
 export const Large = Template.bind({});
 Large.args = {
   size: "large",
-  label: "Button",
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: "small",
   label: "Button",
 };
