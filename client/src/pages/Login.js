33e45e120cde88daa8e9174a41e9ed193cafe767
mariegross <marie.gross@gmx.de>
2020-11-30 16:24:23 +0100

    🚧 Add all components for page layout

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,12 +1,29 @@
+import styled from "styled-components";
 import Input from "../components/Input/Input";
+import bgImg from "../assets/images/background.svg";
+import Button from "../components/Button/Button";
+
+const Wrapper = styled.div`
+  background-image: url(${bgImg});
+  height: 100vh;
+  text-align: center;
+`;
+
+const LoginContainer = styled.div`
+  display: grid;
+`;
 
 function Login() {
   return (
-    <>
+    <Wrapper>
       <h2>Login</h2>
-      <Input placeholder="E-mail address" />
-      <Input placeholder="Password" />
-    </>
+      <LoginContainer>
+        <Input placeholder="E-mail address" />
+        <Input placeholder="Password" />
+        <Button size="large">Login</Button>
+        <Button size="large">Sign up</Button>
+      </LoginContainer>
+    </Wrapper>
   );
 }
 
