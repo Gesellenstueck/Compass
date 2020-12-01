a4adac01de6c908a9ba8d6612943e827e2e29e68
Naasgul <lnaas@hotmail.de>
2020-12-01 17:20:20 +0100

    🚧  Add cards to dashboard page

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,12 +1,45 @@
+import styled from "styled-components/macro";
 import Card from "../components/Card/Card";
+import Teamicon from "../assets/icons/Team.svg";
+import Lockicon from "../assets/icons/Lock.svg";
+import Addicon from "../assets/icons/Add.svg";
+import { ReactComponent as MenuIcon } from "../assets/icons/List.svg";
+
+const Wrapper = styled.div`
+  padding: 2rem;
+  h1,
+  p {
+    color: var(--emphasis-color);
+  }
+
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 56px auto 1fr;
+  grid-column-gap: 1.5rem;
+`;
 
 function Dashboard() {
   return (
-    <>
-      <h2>Login</h2>
-      <p>tbd. </p>
-      <Card type="teamBoard" label="Team Board" />
-    </>
+    <Wrapper>
+      <MenuIcon />
+      <h1>Hello User,</h1>
+      <p>how are you doing today? </p>
+      <Card
+        type="teamBoard"
+        label="Team Board"
+        imgSrc={Teamicon}
+        alt="Team Icon"
+        subline="Marie Groß, Lucas Naas..."
+      />
+      <Card
+        type="privateBoard"
+        label="Private Board"
+        imgSrc={Lockicon}
+        alt="Lock Icon"
+        subline="Marie Groß, Lucas Naas..."
+      />
+      <Card type="addBoard" imgSrc={Addicon} alt="Plus Icon" />
+    </Wrapper>
   );
 }
 
