c0e08f81c1814ecc830f453d8182c94eab312ad6
Naasgul <lnaas@hotmail.de>
2020-12-02 10:38:03 +0100

    💄  Add grid to Card Container on Dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -13,9 +13,21 @@ const Wrapper = styled.div`
   }
 
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  /* grid-template-columns: 1fr 1fr 1fr; */
   grid-template-rows: 56px auto 1fr;
   grid-column-gap: 1.5rem;
+`;
+
+const CardContainer = styled.div`
+  overflow: auto;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 1.5rem;
+
+  div {
+    min-width: 16rem;
+    max-width: 30rem;
+  }
 `;
 
 function Dashboard() {
@@ -24,21 +36,24 @@ function Dashboard() {
       <MenuIcon />
       <h1>Hello User,</h1>
       <p>how are you doing today? </p>
-      <Card
-        type="teamBoard"
-        label="Team Board"
-        imgSrc={Teamicon}
-        alt="Team Icon"
-        subline="Marie Groß, Lucas Naas..."
-      />
-      <Card
-        type="privateBoard"
-        label="Private Board"
-        imgSrc={Lockicon}
-        alt="Lock Icon"
-        subline="Marie Groß, Lucas Naas..."
-      />
-      <Card type="addBoard" imgSrc={Addicon} alt="Plus Icon" />
+
+      <CardContainer>
+        <Card
+          type="teamBoard"
+          label="Team Board"
+          imgSrc={Teamicon}
+          alt="Team Icon"
+          subline="Marie Groß, Lucas Naas..."
+        />
+        <Card
+          type="privateBoard"
+          label="Private Board"
+          imgSrc={Lockicon}
+          alt="Lock Icon"
+          subline="Marie Groß, Lucas Naas..."
+        />
+        <Card type="addBoard" imgSrc={Addicon} alt="Plus Icon" />
+      </CardContainer>
     </Wrapper>
   );
 }
