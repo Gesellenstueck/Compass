3671e2606d0af4616a0d25a1d3e208aaadd73ada
mariegross <marie.gross@gmx.de>
2020-12-03 11:55:58 +0100

    🗃 Add post function and test with postman

--- client/src/api/users.js
+++ client/src/api/users.js
@@ -9,3 +9,15 @@ export async function getUsers() {
   const users = await response.json();
   return users;
 }
+
+export async function postUser(name) {
+  const response = await fetch(`http://localhost:5000/users`, {
+    method: "POST",
+    body: JSON.stringify(name),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const newName = await response.json();
+  return newName;
+}
