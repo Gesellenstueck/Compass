883863281b58008ef80a8088005b15797f8bcbf3
Naasgul <lnaas@hotmail.de>
2020-12-03 11:14:28 +0100

    🗃  Add fake api connection

--- client/src/api/users.js
+++ client/src/api/users.js
@@ -3,3 +3,9 @@ export async function getUserById(id) {
   const user = await response.json();
   return user;
 }
+
+export async function getUsers() {
+  const response = await fetch("http://localhost:5000/users");
+  const users = await response.json();
+  return users;
+}
