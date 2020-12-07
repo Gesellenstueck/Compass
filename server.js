6e42ba032370326ac9a78df9ae9df6fedcb9e159
Naasgul <lnaas@hotmail.de>
2020-12-07 15:52:22 +0100

    🗃  Add post-func to server.js

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+require("dotenv").config();
 const express = require("express");
+
 const path = require("path");
+const { connectToDb, closeDbConnection } = require("./lib/database");
+const { insertUser } = require("./lib/databaseMethods");
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
+app.use(express.json());
 
 // Serve any static files
 app.use(express.static(path.join(__dirname, "client/build")));
@@ -16,6 +21,27 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+app.post("/api/users", async (req, res) => {
+  try {
+    const user = req.body;
+    await insertUser(user);
+    res.send("New User posted into database.");
+  } catch (e) {
+    console.error(e);
+    res
+      .status(500)
+      .send("An unexpected server error occured. Please try again later.");
+  }
 });
+
+async function run() {
+  console.log("Connecting to Database");
+  await connectToDb(process.env.DB_URL, process.env.DB_NAME);
+  console.log("Connected to Database!");
+
+  app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`);
+  });
+}
+
+run();
