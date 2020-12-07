5c5f5cb175f291cff26cf892afe1b43cf12af96e
mariegross <marie.gross@gmx.de>
2020-12-07 17:50:48 +0100

    🚧 Create survey  page

--- client/src/pages/Survey.js
+++ client/src/pages/Survey.js
@@ -1,9 +1,59 @@
+import styled from "styled-components/macro";
+import monsPinkSrc from "../assets/images/monstera-pink.svg";
+import monsYellowSrc from "../assets/images/monstera-yellow.svg";
+import SurveyButton from "../components/Button/SurveyButton";
+
+const Wrapper = styled.div`
+  padding-top: 2rem;
+  display: grid;
+  grid-template-columns: 1fr auto;
+  grid-template-rows: auto 1fr auto;
+`;
+
+const DecoPink = styled.div`
+  background-image: url(${monsPinkSrc});
+  grid-column-start: 2;
+  grid-row-start: 1;
+`;
+
+const DecoYellow = styled.div`
+  background-image: url(${monsYellowSrc});
+  grid-column-start: 1;
+  grid-row-start: 3;
+`;
+
+const QuestionBox = styled.div`
+  grid-row-start: 2;
+  display: grid;
+  grid-template-columns: repeat(7, auto);
+  grid-template-rows: auto 1fr auto;
+
+  button {
+    grid-row-start: 2;
+  }
+`;
+
+const Question = styled.h2`
+  grid-column: 1 / span 7;
+  text-align: center;
+`;
+
 function Survey() {
   return (
-    <>
-      <h2>Survey</h2>
-      <p>tbd. </p>
-    </>
+    <Wrapper>
+      <DecoPink />
+      <QuestionBox>
+        <Question>How was your week?</Question>
+        <SurveyButton />
+        <SurveyButton />
+        <SurveyButton />
+        <SurveyButton size="small" />
+        <SurveyButton />
+        <SurveyButton />
+        <SurveyButton />
+      </QuestionBox>
+      <DecoYellow />
+    </Wrapper>
   );
 }
 
