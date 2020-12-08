3dada1c8002e3a7ca2a10dcf897b63af519b65aa
mariegross <marie.gross@gmx.de>
2020-12-08 14:50:10 +0100

    💄 Fix styled components for for question display

--- client/src/pages/Survey.js
+++ client/src/pages/Survey.js
@@ -4,46 +4,61 @@ import monsYellowSrc from "../assets/images/monstera-yellow.svg";
 import SurveyButton from "../components/Button/SurveyButton";
 
 const Wrapper = styled.div`
-  padding-top: 2rem;
-  display: grid;
-  grid-template-columns: 1fr auto;
-  grid-template-rows: auto 1fr auto;
+  display: block;
+  height: 100vh;
 `;
 
-const DecoPink = styled.div`
-  background-image: url(${monsPinkSrc});
-  grid-column-start: 2;
-  grid-row-start: 1;
-`;
-
-const DecoYellow = styled.div`
-  background-image: url(${monsYellowSrc});
+const MonsPink = styled.img`
   grid-column-start: 1;
   grid-row-start: 3;
+  /* position: relative;
+top: 102%;
+left: -20%; */
+`;
+
+const MonsYellow = styled.img`
+  grid-column-start: 3;
+  grid-row-start: 1;
+  /* position: absolute;
+  top: -20%;
+  left: 102%; */
 `;
 
 const QuestionBox = styled.div`
-  grid-row-start: 2;
   display: grid;
-  grid-template-columns: repeat(7, auto);
-  grid-template-rows: auto 1fr auto;
+  grid-template-columns: 12vw 11vw 10vw 9vw 10vw 11vw 12vw;
+  grid-template-rows: auto auto auto;
   justify-content: center;
-  /* align-items: center; */
+  align-items: center;
+  grid-column-gap: 0.65rem;
+  margin-top: 50vh;
 
-  button {
-    grid-row-start: 2;
-    align-self: center;
+  *:first-child {
+    margin-bottom: 1rem;
+  }
+
+  *:last-child {
+    margin-top: 0.5rem;
   }
 `;
 
 const Question = styled.h2`
+  margin: 0;
   grid-column: 1 / span 7;
+  justify-self: center;
+`;
+
+const Scale = styled.span`
+  color: var(--emphasis-color);
+  font-size: 1.3rem;
+  grid-column-start: ${(props) => (props.value === "1" ? 1 : 7)};
+  text-align: ${(props) => (props.value === "1" ? "left" : "right")};
 `;
 
 function Survey() {
   return (
     <Wrapper>
-      <DecoPink />
+      {/* <MonsYellow src={monsYellowSrc} /> */}
       <QuestionBox>
         <Question>How was your week?</Question>
         <SurveyButton size="large" />
@@ -53,8 +68,10 @@ function Survey() {
         <SurveyButton size="small" />
         <SurveyButton size="medium" />
         <SurveyButton size="large" />
+        <Scale value="1">Great</Scale>
+        <Scale>Bad</Scale>
       </QuestionBox>
-      <DecoYellow />
+      {/* <MonsPink src={monsPinkSrc} /> */}
     </Wrapper>
   );
 }
