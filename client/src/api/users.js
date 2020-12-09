d3e61910128ada4134391ce38d26ce64f293cd0b
Naasgul <lnaas@hotmail.de>
2020-12-09 16:53:35 +0100

    🗃  Change from json-server to mongoDB

--- client/src/api/users.js
+++ client/src/api/users.js
@@ -1,15 +1,3 @@
-export async function getUserById(id) {
-  const response = await fetch(`http://localhost:5000/users/${id}`);
-  const user = await response.json();
-  return user;
-}
-
-export async function getUsers() {
-  const response = await fetch("http://localhost:5000/users");
-  const users = await response.json();
-  return users;
-}
-
 export async function postUser(name) {
   const response = await fetch("api/users", {
     method: "POST",
@@ -20,4 +8,16 @@ export async function postUser(name) {
   });
 
   return response;
+}
+
+export async function getUserById(id) {
+  const response = await fetch(`api/users/${id}`);
+  const user = await response.json();
+  return user;
+}
+
+export async function getUsers() {
+  const response = await fetch("api/users");
+  const users = await response.json();
+  return users;
 }
