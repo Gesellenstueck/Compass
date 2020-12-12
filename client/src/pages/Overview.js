a9004aaa1922a4e3af5b00d9b6740a47c252e04c
Naasgul <lnaas@hotmail.de>
2020-12-12 16:04:11 +0100

    💄  Add very basic structure fpr overview page

--- client/src/pages/Overview.js
+++ client/src/pages/Overview.js
@@ -1,18 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getResults } from "../api/results";
 
 function Overview() {
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+
   useEffect(() => {
     const doFetch = async () => {
       const results = await getResults();
-      console.log(results);
+      console.log(results[71].question);
+      setQuestion(results[71].question);
+      setAnswer(results[71].answer);
     };
     doFetch();
   }, []);
 
   return (
     <>
-      <h2>This weeks mood</h2>
+      <h2>{"This week's mood"}</h2>
+      <h4>{question}</h4>
+      <h4>{answer}</h4>
     </>
   );
 }
