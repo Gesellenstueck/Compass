e68a766e6501950be9aa6cce6de39ac57c533ef6
Naasgul <lnaas@hotmail.de>
2020-12-16 17:45:07 +0100

    💄  Add basic structure for whiteboard page

--- client/src/pages/Whiteboard.js
+++ client/src/pages/Whiteboard.js
@@ -1,8 +1,39 @@
+import Draggable from "react-draggable";
+import { DraggableCard } from "../components/Card/DraggableCard";
+import Back from "../assets/icons/Back.svg";
+import { useHistory } from "react-router-dom";
+import styled from "styled-components/macro";
+import Addicon from "../assets/icons/Add.svg";
+import React from "react";
+
+const Wrapper = styled.div`
+  width: 100vw;
+  min-height: 100vh;
+
+  img {
+    position: fixed;
+    left: 72%;
+    bottom: 3%;
+  }
+`;
+
 function Whiteboard() {
+  const history = useHistory();
   return (
     <>
+      <img src={Back} alt="Back Button" onClick={() => history.goBack()} />
       <h2>Team Board</h2>
-      <p>tbd. </p>
+      <Wrapper>
+        <Draggable bounds="parent">
+          <DraggableCard label="Hallo" bgColor="primaryDark"></DraggableCard>
+        </Draggable>
+
+        <img
+          src={Addicon}
+          alt="Add Icon"
+          onClick={() => React.createElement(DraggableCard)}
+        />
+      </Wrapper>
     </>
   );
 }
