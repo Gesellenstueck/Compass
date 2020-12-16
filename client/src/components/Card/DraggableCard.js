a1d4ffa8e9330793d068743a91bf9aa4ae352766
Naasgul <lnaas@hotmail.de>
2020-12-16 17:44:34 +0100

    💄  Add draggable card as component

---
+++ client/src/components/Card/DraggableCard.js
@@ -0,0 +1,60 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled, { css } from "styled-components/macro";
+
+const bgColors = {
+  primaryDark: css`
+    background: var(--primaryDark);
+  `,
+
+  secondaryLight: css`
+    background: var(--secondaryLight);
+  `,
+  secondaryDark: css`
+    background: var(--secondaryDark);
+  `,
+  primaryLight: css`
+    background: var(--primaryLight);
+  `,
+};
+
+const DragCard = styled.div`
+  border-radius: 1.875rem;
+  padding: 2rem;
+
+  box-shadow: 6px 11px 15px 4px rgba(0, 0, 0, 0.2);
+
+  display: inline-block;
+  ${(props) => bgColors[props.bgColor]};
+  textarea {
+    border: none;
+    background: var(--primaryLight);
+    :focus {
+    }
+  }
+`;
+
+export const DraggableCard = ({ bgColor, label, textarea, ...props }) => {
+  return (
+    <DragCard bgColor={bgColor} {...props}>
+      <h3>{label}</h3>
+
+      <textarea placeholder="What's on your mind?"></textarea>
+    </DragCard>
+  );
+};
+
+DraggableCard.propTypes = {
+  bgColor: PropTypes.oneOf([
+    "primaryDark",
+    "secondaryDark",
+    "primaryLight",
+    "secondaryLight",
+  ]),
+
+  label: PropTypes.string,
+
+  onClick: PropTypes.func,
+
+  textarea: PropTypes.string,
+};
