8a3aab59b1ed1298048d5d9e797b1099ead5084a
Naasgul <lnaas@hotmail.de>
2020-12-16 12:58:33 +0100

    💄  Add basic functionality for result card

--- client/src/components/Card/CardResult.js
+++ client/src/components/Card/CardResult.js
@@ -2,38 +2,55 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled, { css } from "styled-components/macro";
 
+const barSize = {};
+
 const ResultCard = styled.div`
-  height: 100px;
-  width: 100px;
+  color: black;
+  margin-bottom: 2rem;
+
+  dd {
+    width: 100%;
+    margin: 0;
+    background: lightblue;
+    border-radius: 15px;
+    div {
+      width: ${(props) => props.value}%;
+      background: red;
+      border-radius: 15px;
+      text-align: right;
+    }
+  }
 `;
 
-export const ResultContainer = ({ title, value }) => {
-  return (
-    <ResultCard>
-      <h3>{title}</h3>
+const Scale = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
 
-      <span>{value}</span>
+export const ResultContainer = ({ title, value, scale }) => {
+  return (
+    <ResultCard value={value}>
+      <h3>{title}</h3>
+      <dd>
+        <div>{value}%</div>
+      </dd>
+      <Scale>
+        <span> {scale[0]}</span>
+        <span> {scale[1]}</span>
+      </Scale>
     </ResultCard>
   );
 };
 
 ResultContainer.propTypes = {
-  bgColor: PropTypes.oneOf([
-    "primaryDark",
-    "secondaryDark",
-    "primaryLight",
-    "secondaryLight",
-  ]),
-
   title: PropTypes.string,
 
   onClick: PropTypes.func,
 
-  value: PropTypes.string,
+  value: PropTypes.number,
 
-  alt: PropTypes.string,
+  scale: PropTypes.string,
 
   subline: PropTypes.string,
-
-  iconProps: PropTypes.oneOf(["teamIcon"]),
 };
