1e6aba301fa094bac38d11857b2483ff149013df
mariegross <marie.gross@gmx.de>
2020-12-17 16:12:52 +0100

    🚧 Build add function for draggable cards

--- client/src/pages/Whiteboard.js
+++ client/src/pages/Whiteboard.js
@@ -1,10 +1,11 @@
-import Draggable from "react-draggable";
 import { DraggableCard } from "../components/Card/DraggableCard";
 import Back from "../assets/icons/Back.svg";
 import { useHistory } from "react-router-dom";
 import styled from "styled-components/macro";
 import Addicon from "../assets/icons/Add.svg";
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Wrapper = styled.div`
   width: 100vw;
@@ -19,19 +20,44 @@ const Wrapper = styled.div`
 
 function Whiteboard() {
   const history = useHistory();
+  const [cards, setCards] = useState([]);
+  const [carsArray, setCardsArray] = useState([]);
+
+  useEffect(() => {
+    console.log("render");
+  }, [cards]);
+
+  const createCard = () => {
+    for (let i = 0; ; i++) {
+      let cardsArray = ["0"];
+      cardsArray.push(i);
+      console.log(cardsArray);
+    }
+
+    // const card = this.state.cardArray;
+    // const key = "";
+    // const props = "";
+    // card.push({ key, ...props });
+    // this.setState({ cardArray: card });
+  };
+
+  // const cardsArray = []
+  // cardsArray.push(DraggableCard)
+
   return (
     <>
       <img src={Back} alt="Back Button" onClick={() => history.goBack()} />
       <h2>Team Board</h2>
       <Wrapper>
-        <Draggable bounds="parent">
-          <DraggableCard label="Hallo" bgColor="primaryDark"></DraggableCard>
-        </Draggable>
-
+        {cards.map((index) => {
+          return (
+            <DraggableCard key={index} label="Hallo" bgColor="primaryDark" />
+          );
+        })}
         <img
           src={Addicon}
           alt="Add Icon"
-          onClick={() => React.createElement(DraggableCard)}
+          onClick={(key, props) => createCard(key, props)}
         />
       </Wrapper>
     </>
