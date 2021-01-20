8f5f0314b936943cdac4781e4aefc7cedcf16a5a
mariegross <marie.gross@gmx.de>
2021-01-20 16:47:53 +0100

    🚑 Fix errors for node.js CI test to pass

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -1,15 +1,6 @@
 import PropTypes from "prop-types";
 import styled from "styled-components/macro";
 
-const sizes = {
-  small: {
-    font: "Oxygen-Regular",
-  },
-  large: {
-    font: "Oxygen-Bold",
-  },
-};
-
 const Button = styled.button`
   border-radius: 1.875rem;
   letter-spacing: 0.07rem;
@@ -25,9 +16,6 @@ const Button = styled.button`
 
 Button.propTypes = {
   backgroundColor: PropTypes.string,
-
-  size: PropTypes.oneOf(["small", "large"]),
-
   onClick: PropTypes.func,
 };
 
